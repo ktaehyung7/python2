f13708052f4597b16afe1490e6d446eae986b4a0
ktaehyung7 <55673916+ktaehyung7@users.noreply.github.com>
2021-04-04 17:16:18 +0900

    Add files via upload

---
+++ history.py
@@ -0,0 +1,104 @@
+df.loc['20200311':].shift(1)
+df1.loc['20200311':].shift(1)
+df1.loc['20200311':]
+test_sc_df['20200312'] = [ 1,0,0,0,0,0,0,0,0,0,0,0,1]
+test_sc_df['20200312'] = np.nan
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+test_sc_df['20200312'] = np.nan
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+test_sc_df.loc['20200312'] = np.nan
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Mon May 18 23:54:57 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/untitled2.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 00:02:32 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 00:09:44 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 00:23:35 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 21:10:02 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+clear
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 21:36:27 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 21:38:35 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Tue May 19 21:46:47 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Fri May 22 23:22:17 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex01.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Sun Jun  7 16:03:46 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Wed Jun 10 23:12:29 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001_00.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Fri Jun 12 21:08:46 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001_00.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Sat Jun 13 15:16:48 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001_00.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Fri Jun 19 15:29:13 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex01.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex02.py', wdir='C:/Users/nerguri/.spyder-py3')
+print(db)
+print(dir(db))
+print(db.name)
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex02.py', wdir='C:/Users/nerguri/.spyder-py3')
+test_collection
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex02.py', wdir='C:/Users/nerguri/.spyder-py3')
+post_id
+test_collection.count()
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex02.py', wdir='C:/Users/nerguri/.spyder-py3')
+test_collection.count()
+test_collection.find()
+runfile('C:/Users/nerguri/.spyder-py3/mongo_ex02.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Mon Jun 22 09:15:06 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/openpyxl_ex_001.py', wdir='C:/Users/nerguri/.spyder-py3')
+print(load_ws.cell(1,2).value)
+runfile('C:/Users/nerguri/.spyder-py3/openpyxl_ex_001.py', wdir='C:/Users/nerguri/.spyder-py3')
+clear
+runfile('C:/Users/nerguri/.spyder-py3/openpyxl_ex_002.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Sat Jun 27 14:51:41 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/test.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/.spyder-py3/lstm_exam001_00.py', wdir='C:/Users/nerguri/.spyder-py3')
+
+## ---(Sun Jul 12 16:39:10 2020)---
+runfile('C:/Users/nerguri/.spyder-py3/AAPL_lstm_keras.py', wdir='C:/Users/nerguri/.spyder-py3')
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+df_price.columns
+df_price.describe()
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+df_scaled.columns = scale_cols
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+clear
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+x_train.shape, x_valid.shape
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+test[feature_cols]
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+test_feature.shape, test_label.shape
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
+
+## ---(Fri Jul 31 14:28:27 2020)---
+runfile('C:/Users/nerguri/APPL_letm_exam.py', wdir='C:/Users/nerguri')
